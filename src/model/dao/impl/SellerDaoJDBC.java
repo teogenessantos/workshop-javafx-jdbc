a97853f5cf004709ac9db8c66464bd52a374f390
Teogenes <teogenes.santos@gmail.com>
2020-04-22 16:00:37 -0300

    TextField & DatePicker

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -150,7 +150,7 @@ public class SellerDaoJDBC implements SellerDao{
 		 seller.setId(rs.getInt("Id"));
 		 seller.setName(rs.getString("Name"));
 		 seller.setEmail(rs.getString("Email"));
-		 seller.setBirthDate(rs.getDate("BirthDate"));
+		 seller.setBirthDate(new java.util.Date(rs.getTimestamp("BirthDate").getTime()));
 		 seller.setBaseSalary(rs.getDouble("BaseSalary"));
 		 seller.setDepartment(department);
 		 return seller;
